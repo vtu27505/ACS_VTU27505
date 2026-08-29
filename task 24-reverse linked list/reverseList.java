940f7e8c8f6597eb9641e81124236fd63d4ebbba
JASWSANTH KUMAR REDDY <vtu27505@veltech.edu.in>
2026-08-29 09:45:58 +0530

    Create reverseList.java

---
+++ task 24-reverse linked list/reverseList.java
@@ -0,0 +1,51 @@
+public class ReverseLinkedList {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+
+        while (current != null) {
+            Node next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+
+    public static void main(String[] args) {
+
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+        System.out.println("Original List:");
+        print(head);
+
+        head = reverse(head);
+
+        System.out.println("Reversed List:");
+        print(head);
+    }
+
+    public static void print(Node head) {
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+}
